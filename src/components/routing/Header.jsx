cd1a06d5a0c6d597af8d775cbb238095fb81c8d1
Matas Liutikas <matasliutikas@gmail.com>
2021-04-14 21:40:59 +0300

    added nice styling to header

--- src/components/routing/Header.jsx
+++ src/components/routing/Header.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import "./Header.less"
 
 
 function Header() {
     return (
-        <div className='header-container'>
-            <Link className="routing-item" to='/'>Home</Link>
-            <Link className="routing-item" to='/map'>Map</Link>
-            <Link className="routing-item" to='/map'>Map</Link>
+        <div className="header">
+            <div className='routing-container'>
+                <NavLink exact to='/' className="routing-item" activeClassName="active">Home</NavLink>
+                <NavLink to='/map' className="routing-item" activeClassName="active">Map</NavLink>
+                <NavLink to='/products' className="routing-item" activeClassName="active">Products</NavLink>
+            </div>
+            <div className='account-container'>
+                <NavLink exact to='/' className="routing-item" activeClassName="active">Login</NavLink>
+                <NavLink exact to='/' className="routing-item" activeClassName="active">Register</NavLink>
+            </div>
         </div>
     );
 }
