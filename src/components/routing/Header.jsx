a5c8ff6db03ae06cab116226a1224a8ce8114645
Matas Liutikas <matasliutikas@gmail.com>
2021-04-14 22:24:18 +0300

    added more entries to header

--- src/components/routing/Header.jsx
+++ src/components/routing/Header.jsx
@@ -7,13 +7,15 @@ function Header() {
     return (
         <div className="header">
             <div className='routing-container'>
-                <NavLink exact to='/' className="routing-item" activeClassName="active">Home</NavLink>
-                <NavLink to='/map' className="routing-item" activeClassName="active">Map</NavLink>
+                <NavLink exact to='/' className="routing-item" activeClassName="active">Map</NavLink>
                 <NavLink to='/products' className="routing-item" activeClassName="active">Products</NavLink>
+                <NavLink to='/scan' className="routing-item" activeClassName="active">Scan</NavLink>
+                <NavLink to='/trends' className="routing-item" activeClassName="active">Trends</NavLink>
+                <NavLink to='/wishlist' className="routing-item" activeClassName="active">Wishlist</NavLink>
             </div>
             <div className='account-container'>
-                <NavLink exact to='/' className="routing-item" activeClassName="active">Login</NavLink>
-                <NavLink exact to='/' className="routing-item" activeClassName="active">Register</NavLink>
+                <NavLink exact to='/login' className="routing-item" activeClassName="active">Login</NavLink>
+                <NavLink exact to='/register' className="routing-item" activeClassName="active">Register</NavLink>
             </div>
         </div>
     );
