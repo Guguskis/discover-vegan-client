3df4d109b5145b1f0031be2331ec6655bf8997ce
Matas Liutikas <matasliutikas@gmail.com>
2021-04-15 21:21:36 +0300

    changed vendor marker style

--- src/components/map/VendorMarkers.jsx
+++ src/components/map/VendorMarkers.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Marker} from "react-map-gl";
 import StorefrontIcon from "@material-ui/icons/Storefront";
+import IconButton from "@material-ui/core/IconButton";
+import "./VendorMarkers.less"
 
 const VendorMarkers = (props) => {
     const {vendors, setPopupInfo} = props;
@@ -10,12 +12,13 @@ const VendorMarkers = (props) => {
             key={vendor.id}
             longitude={vendor.position.longitude}
             latitude={vendor.position.latitude}
-            offsetTop={-20} // center
-            offsetLeft={-20} // center
+            offsetTop={-26} // center
+            offsetLeft={-26} // center
+            className="vendor-marker"
         >
-            <button className='vendorMarker' onClick={() => setPopupInfo(vendor)}>
+            <IconButton onClick={() => setPopupInfo(vendor)}>
                 <StorefrontIcon className='vendorIcon' width='auto' height='auto'/>
-            </button>
+            </IconButton>
         </Marker>
     ))
 };
