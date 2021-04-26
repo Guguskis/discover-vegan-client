188f167aa49c73be0d65f54c1313dc6c59828572
Matas Liutikas <matasliutikas@gmail.com>
2021-04-26 22:12:10 +0300

    displaying name, price and icons for ProductVendorsList.jsx

--- src/components/common/ProductVendorsList.jsx
+++ src/components/common/ProductVendorsList.jsx
@@ -2,41 +2,48 @@ import React from 'react';
 import ListItem from "@material-ui/core/ListItem";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
-import LocalGroceryStoreIcon from "@material-ui/icons/LocalGroceryStore.js";
+import MapIcon from '@material-ui/icons/Map';
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import IconButton from "@material-ui/core/IconButton";
-import EditIcon from "@material-ui/icons/Edit.js";
-import DeleteIcon from "@material-ui/icons/Delete.js";
 import List from "@material-ui/core/List";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+
+import "./ProductVendorsList.less"
 
 const ProductVendorsList = (props) => {
     const {productVendorDetails} = props;
 
     return (
         <List dense={false} className="manage-products-container">
-            {productVendorDetails.map((detail) =>
-                React.cloneElement(<ListItem>
-                    <ListItemAvatar>
-                        <Avatar>
-                            <LocalGroceryStoreIcon/>
-                        </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText
-                        className="list-item-text"
-                        primary={detail.product.name}
-                    />
-                    <ListItemSecondaryAction>
-                        <IconButton edge="end" aria-label="edit">
-                            <EditIcon/>
-                        </IconButton>
-                        <IconButton edge="end" aria-label="delete">
-                            <DeleteIcon/>
-                        </IconButton>
-                    </ListItemSecondaryAction>
-                </ListItem>, {
-                    key: detail.vendor.vendorId,
-                }),
+            {productVendorDetails.map((detail) => {
+                    const vendor = detail.vendor;
+                    const product = detail.product;
+                    const price = `${product.price} €`;
+
+                    return (
+                        React.cloneElement(<ListItem>
+                            <ListItemAvatar>
+                                <Avatar className="store-icon">
+                                    <StorefrontIcon/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText
+                                className="list-item-text"
+                                primary={vendor.name}/>
+                            <ListItemText
+                                className="list-item-text"
+                                primary={price}/>
+                            <ListItemSecondaryAction>
+                                <IconButton edge="end" aria-label="edit">
+                                    <MapIcon/>
+                                </IconButton>
+                            </ListItemSecondaryAction>
+                        </ListItem>, {
+                            key: vendor.vendorId,
+                        })
+                    )
+                }
             )}
         </List>
     );
