17721ebd86c7ad063b651c0b8b7d0cc8706e544e
Matas Liutikas <matasliutikas@gmail.com>
2021-05-06 23:13:14 +0300

    connected to api

--- src/components/common/ProductTrendAnalysis.jsx
+++ src/components/common/ProductTrendAnalysis.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
+import "date-utils";
 import "./ProductTrendAnalysis.less"
 import {CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, XAxis, YAxis} from "recharts";
 import Tooltip from "@material-ui/core/Tooltip";
+import {API} from "../../config/axiosConfig.jsx";
 
 
 const ProductTrendAnalysis = (props) => {
     const {product} = props;
+
+    const [fromDate, setFromDate] = useState(Date.today().add({days: -3}))
+    const [toDate, setToDate] = useState(Date.today())
+    const [searchRequests, setSearchRequests] = useState([])
+
+    const [{data: productSearchRequestsData, loading: productSearchRequestsLoading, error: productSearchRequestsError}, executeProductSearchRequests] = API.useDiscoverVeganApiAxios(
+        {
+            url: `/api/trend/${product?.productId}`,
+            method: 'GET',
+            params: {
+                fromDate: fromDate.toFormat("YYYY-MM-DD"),
+                toDate: toDate.toFormat("YYYY-MM-DD"),
+                stepCount: 20
+            }
+        }, {manual: true}
+    )
+
+    useEffect(() => {
+        if (product) {
+            executeProductSearchRequests()
+        }
+    }, [product])
+
+    useEffect(() => {
+        if (!productSearchRequestsLoading && productSearchRequestsData) {
+            setSearchRequests(productSearchRequestsData)
+        }
+    }, [productSearchRequestsLoading])
 
     const data = [
         {
@@ -72,8 +102,8 @@ const ProductTrendAnalysis = (props) => {
                     <YAxis/>
                     <Tooltip/>
                     <Legend/>
-                    <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
-                    <Line type="monotone" dataKey="uv" stroke="#82ca9d"/>
+                    <Line type="monotone" dataKey="pv" stroke="#000000" activeDot={{r: 8}}/>
+                    <Line type="monotone" dataKey="uv" stroke="#ffffff"/>
                 </LineChart>
             </ResponsiveContainer>
         </div>
