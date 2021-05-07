0f422c14a48ddf3c095c8559439148cc1e9adaef
Matas Liutikas <matasliutikas@gmail.com>
2021-05-07 12:09:30 +0300

    updated nedpoint url

--- src/components/common/ProductTrendAnalysis.jsx
+++ src/components/common/ProductTrendAnalysis.jsx
@@ -17,7 +17,7 @@ const ProductTrendAnalysis = (props) => {
 
     const [{data: productSearchRequestsData, loading: productSearchRequestsLoading, error: productSearchRequestsError}, executeProductSearchRequests] = API.useDiscoverVeganApiAxios(
         {
-            url: `/api/trend/${product?.productId}`,
+            url: `/api/trend/${product?.productId}/search`,
             method: 'GET',
             params: {
                 fromDate: fromDate.toFormat("YYYY-MM-DD"),
